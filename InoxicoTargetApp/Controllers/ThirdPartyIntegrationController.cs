e46461d3ed3728b5ab6a04a1be94fb42327e5bea
dandrejvv <dandre.jvv@intentarchitect.com>
2020-03-12 17:21:50 +0200

    Fixes the styling

---
+++ InoxicoTargetApp/Controllers/ThirdPartyIntegrationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InoxicoTargetApp.Controllers
+{
+    public class ThirdPartyIntegrationController : Controller
+    {
+        public ActionResult GetRedirectUrl(string clientId)
+        {
+
+            return Json(new { Url = "" });
+        }
+    }
+}
