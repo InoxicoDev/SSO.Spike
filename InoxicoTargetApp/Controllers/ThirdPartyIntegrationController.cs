132ba7dbe54d909b0c5e716d98ffca7bd37a13fd
dandrejvv <dandre.jvv@intentarchitect.com>
2020-03-17 14:59:35 +0200

    Can now send and receive token headers

--- InoxicoTargetApp/Controllers/ThirdPartyIntegrationController.cs
+++ InoxicoTargetApp/Controllers/ThirdPartyIntegrationController.cs
@@ -1,6 +1,7 @@
 ﻿using IdentityModel.Client;
 using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
@@ -15,9 +16,18 @@ namespace InoxicoTargetApp.Controllers
         [HttpPost]
         public async Task<string> AuthenticateExternalUser(string clientId)
         {
-            var response = await _httpClient.PutAsync($"https://localhost:44301/api/ReferenceCode/{clientId}", new StringContent(string.Empty));
+            var idToken = this.Request.Headers["id"];
+
+            var request = new HttpRequestMessage(HttpMethod.Put, $"https://localhost:44301/api/ReferenceCode/{clientId}");
+            request.Headers.Add("id", idToken);
+            request.Content = new StringContent(string.Empty);
+
+            var response = await _httpClient.SendAsync(request);
             response.EnsureSuccessStatusCode();
+
             var refCode = await response.Content.ReadAsStringAsync();
+            refCode = refCode.Replace("\"", string.Empty);
+
             return $"{this.Request.Url.Scheme}://{this.Request.Url.Authority}/{refCode}";
         }
     }
